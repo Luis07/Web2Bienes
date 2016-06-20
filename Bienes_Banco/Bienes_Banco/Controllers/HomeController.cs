8a726b196f5be95ce6dfce57570b41519b1bc6ca
Luis07 <luisbrenes250594@gmail.com>
2016-06-20 14:27:34 -0600

    Se eliminaron carpetas inutiles

--- Bienes_Banco/Bienes_Banco/Controllers/HomeController.cs
+++ Bienes_Banco/Bienes_Banco/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,6 +21,25 @@ namespace Bienes_Banco.Controllers
             ViewBag.Title = "Contactanos";
 
             return View();
+        }
+        // POST: Home/Contacto
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contacto([Bind(Include = "id,name,lastname,admin,email,password,ubicacion,activo,bloqueado")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                    db.users.Add(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                
+                    ViewData["Error"] = "El correo ya existe";
+            }
+
+            return View(user);
         }
     }
 }
