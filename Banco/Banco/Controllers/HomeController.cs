ee57e9c81fa3f7a1a6723d6c4d7589fa59614a58
Luis07 <luisbrenes250594@gmail.com>
2016-07-01 22:24:18 -0600

    se agrego codigo para envio de correos de informacion de contactos

--- Banco/Banco/Controllers/HomeController.cs
+++ Banco/Banco/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Banco.Models;
+using Bienes_Banco.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +12,7 @@ namespace Banco.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationUserManager _userManager;
         public ActionResult Index()
         {
             return View();
@@ -25,6 +30,79 @@ namespace Banco.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+         // POST: Home/Contacto
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact([Bind(Include = "name,lastname,email,asunto,contenido")] user user)
+        {
+            if (EnviarCorreo(user)) {
+                return View(user);
+            }         
+                return View(user);
+        }
+        public bool EnviarCorreo(user user)
+        {
+            /*-------------------------MENSAJE DE CORREO----------------------*/
+
+            //Creamos un nuevo Objeto de mensaje
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+
+            //Direccion de correo electronico a la que queremos enviar el mensaje
+            mmsg.To.Add("luisbrenes250594@gmail.com");
+
+            //Nota: La propiedad To es una colección que permite enviar el mensaje a más de un destinatario
+
+            //Asunto
+            mmsg.Subject = user.asunto.ToString();
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            //Direccion de correo electronico que queremos que reciba una copia del mensaje
+            mmsg.Bcc.Add("greivindca7@gmail.com"); //Opcional
+
+            //Cuerpo del Mensaje
+            mmsg.Body = user.contenido;
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true; //Si no queremos que se envíe como HTML
+
+            //Correo electronico desde la que enviamos el mensaje
+            mmsg.From = new System.Net.Mail.MailAddress(user.email);
+
+
+            /*-------------------------CLIENTE DE CORREO----------------------*/
+
+            //Creamos un objeto de cliente de correo
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+
+            //Hay que crear las credenciales del correo emisor
+            cliente.Credentials =
+                new System.Net.NetworkCredential("greivindca7@gmail.com", "gredan0708$");
+
+            //Lo siguiente es obligatorio si enviamos el mensaje desde Gmail
+            
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+            
+
+            cliente.Host = "smtp.gmail.com"; //Para Gmail "smtp.gmail.com";
+
+
+            /*-------------------------ENVIO DE CORREO----------------------*/
+
+            try
+            {
+                //Enviamos el mensaje      
+                cliente.Send(mmsg);
+                return true;
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+                ex.ToString();
+                return false;
+                //Aquí gestionamos los errores al intentar enviar el correo
+            }
         }
     }
 }
